1a0f5d7b0dfe90ce9ed647b937ebdb7959e907a9
PauloB <paulo.bernardino2@gmail.com>
2023-01-13 14:44:24 +0000

    Adicionar Jogos e competições

---
+++ lib/data/jogos.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Games {
+  final DateTime gameStarts;
+  final String homeTeam;
+  final String outsiderTeam;
+
+  Games({
+    required this.gameStarts,
+    required this.homeTeam,
+    required this.outsiderTeam,
+  });
+}
---
+++ lib/populating/competitions.dart
@@ -0,0 +1,14 @@
+import 'package:trabalho2/data/competition.dart';
+import 'package:trabalho2/data/jogadores.dart';
+
+final List<Competition> competitions = [
+  Competition(
+    nameCompetition: 'Primeira Liga',
+  ),
+  Competition(
+    nameCompetition: 'Segunda Liga',
+  ),
+  Competition(
+    nameCompetition: 'Terceira Liga',
+  ),
+];
