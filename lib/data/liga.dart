d907af9d467e5090cb79a508b9783a0e8f587ca6
PauloB <paulo.bernardino2@gmail.com>
2023-01-13 15:27:45 +0000

    class liga

---
+++ lib/data/liga.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Liga {
+  final DateTime ligaStart;
+  final int durationMonths;
+  final double prize;
+
+  Liga({
+    required this.ligaStart,
+    required this.durationMonths,
+    required this.prize,
+  });
+}
