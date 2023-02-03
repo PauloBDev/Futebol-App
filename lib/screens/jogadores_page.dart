5c4550612f0411bba324d886c8fa68ddcb0566c8
PauloB <paulo.bernardino2@gmail.com>
2023-02-03 15:27:53 +0000

    text fields

--- lib/screens/jogadores_page.dart
+++ lib/screens/jogadores_page.dart
@@ -38,24 +38,30 @@ class _JogadoresPageState extends State<JogadoresPage> {
         ),
         child: ListView(
           children: jogadoresDeClube.map((e) {
-            return Card(
-              child: Row(
-                children: [
-                  Image.asset(
-                    'images/profile_pic.png',
-                    width: 100,
-                    height: 100,
-                  ),
-                  Container(
-                    margin: const EdgeInsets.all(5),
-                    child: Text(
-                      '''Nome: ${e.namejogador}
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.asset(
+                        'images/profile_pic.png',
+                        width: 100,
+                        height: 100,
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(5),
+                      child: Text(
+                        '''Nome: ${e.namejogador}
 Idade: ${e.idade}
 Clube: ${e.nameClube}
 Contratação: ${e.date}''',
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             );
           }).toList(),
